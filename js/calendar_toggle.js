ddba993908cf0f8b18dde903b46f96febf67ac7a
Eric Wu <ericwu1997@gmail.com>
2017-05-10 07:29:41 +0800

    HTML & CSS barebone

---
+++ js/calendar_toggle.js
@@ -0,0 +1,13 @@
+var calander_tab_on = true;
+$(document).ready(function() {
+    $("#expand_tab").click(function() {
+        $("#calendar_container").slideToggle("slow");
+        if (calander_tab_on) {
+            calander_tab_on = false;
+            $("#expand_tab").html("&#9660;");
+        } else {
+            $("#expand_tab").html("&#9650;");
+            calander_tab_on = true;
+        }
+    });
+});
