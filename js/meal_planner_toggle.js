1feac3937fc1e5bcfc9320839a5b3fabd0eb5557
Eric Wu <ericwu1997@gmail.com>
2017-05-13 02:43:56 +0800

    affiliates drop down

--- js/meal_planner_toggle.js
+++ js/meal_planner_toggle.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var meal_time_title = true;
     var meal_option_tab = false;
+    var affiliates_tab = false;
     var edit_meal = "";
     if ($(window).width() > 600) {
         $("#calendar").on("click", "td", function(event) {  
@@ -86,6 +87,16 @@ $(document).ready(function() {
             console.log(event.target.id);
         });
     } else {
+        $("#mobile_home_page_nav").on("click", "img", function() {
+            if(!affiliates_tab){
+                $("#mobile_home_page_nav").find('img').css({'background-color' : '#719c6e','box-shadow' : 'inset 0px 0px 10px rgba(0,0,0,0.5)'});
+                affiliates_tab = true;
+            } else {
+                $("#mobile_home_page_nav").find('img').css({'background-color' : '#8DC289','box-shadow' : 'none'});
+                affiliates_tab = false;
+            }
+            $("#affiliates").slideToggle("slow");
+        });
         $("#calendar").on("click", "td", function(event) {  
           $("#calendar").find('*').removeClass('selected_date');
           $(event.target).addClass('selected_date');
