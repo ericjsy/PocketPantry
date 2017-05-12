8f6aa1d4ad6698f3f33c540c46cf0cc13b904e44
ericjsy <e.sy@oceans.ubc.ca>
2017-05-11 19:24:57 -0700

    Affiliates aesthic fix for MOBILE

--- js/affiliates.js
+++ js/affiliates.js
@@ -1,15 +1,15 @@
 var educational_tab_on = true;
 var behavioral_tab_on = true;
-var awareness_tab_on = true;
+var shopping_tab_on = true;
 
 $(document).ready(function() {
     $("#expand_educational").click(function() {
         $("#educational_block").slideToggle("slow");
         if (educational_tab_on) {
             educational_tab_on = false;
-            $("#expand_educational").html("&#9660; Educational");
+            $("#expand_educational").html('Educational <img src="img/Arrowdown.png" alt="collapse_arrow" height="52" width="72">');
         } else {
-            $("#expand_educational").html("&#9650; Educational");
+            $("#expand_educational").html('Educational <img src="img/Arrowup.png" alt="collapse_arrow" height="52" width="72">');
             educational_tab_on = true;
         }
     });
@@ -17,20 +17,20 @@ $(document).ready(function() {
         $("#behavioral_block").slideToggle("slow");
         if (behavioral_tab_on) {
             behavioral_tab_on = false;
-            $("#expand_behavioral").html("&#9660; Behavioral");
+            $("#expand_behavioral").html('Behavioral <img src="img/Arrowdown.png" alt="collapse_arrow" height="52" width="72">');
         } else {
-            $("#expand_behavioral").html("&#9650; Behavioral");
+            $("#expand_behavioral").html('Behavioral <img src="img/Arrowup.png" alt="collapse_arrow" height="52" width="72">');
             behavioral_tab_on = true;
         }
     });
-	$("#expand_awareness").click(function() {
-        $("#awareness_block").slideToggle("slow");
-        if (awareness_tab_on) {
-            awareness_tab_on = false;
-            $("#expand_awareness").html("&#9660; Awareness");
+	$("#expand_shopping").click(function() {
+        $("#shopping_block").slideToggle("slow");
+        if (shopping_tab_on) {
+            shopping_tab_on = false;
+            $("#expand_shopping").html('Shopping <img src="img/Arrowdown.png" alt="collapse_arrow" height="52" width="72">');
         } else {
-            $("#expand_awareness").html("&#9650; Awareness");
-            awareness_tab_on = true;
+            $("#expand_awareness").html('Shopping <img src="img/Arrowup.png" alt="collapse_arrow" height="52" width="72">');
+            shopping_tab_on = true;
         }
     });
 });
