6b245f9671b2f8e51f999cfd14ea6689fe5d4d0d
Ryalia888 <ryalia888@gmail.com>
2017-05-17 12:05:40 -0700

    Creating user

--- js/detectUserData.js
+++ js/detectUserData.js
@@ -5,10 +5,11 @@ window.onload = firebase.auth().onAuthStateChanged(function(user) {
 			name = user.displayName;
 			uid = user.uid;
 			console.log("Welcome " + name);
+			connectUser(uid);
 			//window.location.replace("meal_planner.html");
 		}
 	}
 	else {
-			window.location.replace("sign_in.html");
+		window.location.replace("sign_in.html");
 	}
 });
