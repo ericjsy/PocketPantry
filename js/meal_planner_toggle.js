72ff6a405b6a1259dc6b34258606db1ee1075859
Ryalia888 <ryalia888@gmail.com>
2017-05-18 11:52:59 -0700

    logodropdown fixed, separated & updated css

--- js/meal_planner_toggle.js
+++ js/meal_planner_toggle.js
@@ -1,21 +1,9 @@
 $(document).ready(function() {
     var meal_time_title = true;
     var meal_option_tab = false;
-    var affiliates_tab = false;
     var selectedDate = "";
     var edit_meal = "";
     
-    // Affiliates page navigation toggle
-    $("#mobile_home_page_nav").on("click", "img", function() {
-        if(!affiliates_tab){
-            $("#mobile_home_page_nav").find('img').css({'background-color' : '#719c6e','box-shadow' : 'inset 0px 0px 10px rgba(0,0,0,0.5)'});
-            affiliates_tab = true;
-        } else {
-            $("#mobile_home_page_nav").find('img').css({'background-color' : '#8DC289','box-shadow' : 'none'});
-            affiliates_tab = false;
-        }
-        $("#affiliates").stop(true, false).slideToggle("slow");
-    });
     // PC Meal Planner edit mode exit
     $("#PC_exit").on("click", function() {
         $("#meal_builder").css("display", "none");
