7d807d72ed283bf7182497a65074c963b25416e0
Elizabeth Lee <quotable01@gmail.com>
2017-05-25 17:25:55 -0700

    Fixed opening and closing the dropdown

--- js/dropdown_menu.js
+++ js/dropdown_menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#dropdown_toggle").on("click", "img", function(event) {
+    $("#dropdown_toggle").on("click", "img, div", function(event) {
         $("#dropdown_content").stop().slideToggle("slow");
     });
 });
